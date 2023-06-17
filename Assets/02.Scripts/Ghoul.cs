853937ee0fcf811effa4d09e6bcf07ef8d448e26
Ojunsun <98933734+Ojunsun@users.noreply.github.com>
2023-06-18 04:04:21 +0900

    Enemy
    Move&Ending

--- Assets/02.Scripts/Ghoul.cs
+++ Assets/02.Scripts/Ghoul.cs
@@ -1,8 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class Ghoul : MonoBehaviour
 {
-    
+    public Transform player;
+    public Vector3 specificPosition;
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= 60f)
+        {
+            agent.SetDestination(player.position);
+            FaceTarget(player.position);
+        }
+        else
+        {
+            agent.SetDestination(specificPosition);
+            FaceTarget(specificPosition);
+        }
+    }
+
+    void FaceTarget(Vector3 targetPosition)
+    {
+        Vector3 direction = (targetPosition - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Ending");
+        }
+    }
 }
